b5ae49967d2ff377c0af68d4f14792e98c975753
Sultonbek Rakhimov <sultonbek.rakhimov@gmail.com>
2025-02-22 23:34:22 +0500

    [B-5] Refactored enum range extensions

--- src/Backbone.Language.Core.Extensions.Enums.Range/Attributes/EnumRangeAttribute.cs
+++ src/Backbone.Language.Core.Extensions.Enums.Range/Attributes/EnumRangeAttribute.cs
@@ -10,32 +10,4 @@ public sealed class EnumRangeAttribute(ushort range) : Attribute
     /// Gets the range value specified for the enum.
     /// </summary>
     public ushort Range { get; } = range;
-
-    // /// <summary>
-    // /// Validates and retrieves the <see cref="EnumRangeAttribute"/> applied to the specified enum type.
-    // /// Throws an exception if the attribute is not found.
-    // /// </summary>
-    // /// <typeparam name="TEnum">The enum type to validate.</typeparam>
-    // /// <param name="enumRange">The retrieved <see cref="EnumRangeAttribute"/>.</param>
-    // /// <exception cref="InvalidOperationException">Thrown when the enum does not have the <see cref="EnumRangeAttribute"/> applied.</exception>
-    // public static void ValidateAndThrow<TEnum>(out EnumRangeAttribute enumRange) where TEnum : Enum => 
-    //     ValidateAndThrow(typeof(TEnum), out enumRange);
-    //
-    // /// <summary>
-    // /// Validates and retrieves the <see cref="EnumRangeAttribute"/> applied to the specified enum type.
-    // /// Throws an exception if the attribute is not found.
-    // /// </summary>
-    // /// <param name="enumType">The enum type to validate.</param>
-    // /// <param name="enumRange">The retrieved <see cref="EnumRangeAttribute"/>.</param>
-    // /// <exception cref="InvalidOperationException">Thrown when the enum does not have the <see cref="EnumRangeAttribute"/> applied.</exception>
-    // public static void ValidateAndThrow(Type enumType, out EnumRangeAttribute enumRange)
-    // {
-    //     if (!enumType.IsEnum)
-    //         throw new ArgumentException($"Type {enumType.Name} must be an enum");
-    //     
-    //     if (enumType.GetCustomAttributes(typeof(EnumRangeAttribute), false).FirstOrDefault() is not EnumRangeAttribute rangeValue)
-    //         throw new InvalidOperationException($"Enum {enumType.Name} must be range enum");
-    //
-    //     enumRange = rangeValue;
-    // }
 }
--- src/Backbone.Language.Core.Extensions.Enums.Range/Extensions/EnumRangeExtensions.cs
+++ src/Backbone.Language.Core.Extensions.Enums.Range/Extensions/EnumRangeExtensions.cs
@@ -1,96 +1,64 @@
-// namespace Backbone.Language.Core.Extensions.Enums.Range.Extensions;
-//
-// /// <summary>
-// /// Contains enum range extensions.
-// /// </summary>
-// public static class EnumRangeExtensions
-// {
-//     #region Definition Validation
-//
-//     /// <summary>
-//     /// Validates if the enum type is a proper range enum based on the <see cref="RangeEnumAttribute"/> applied to it.
-//     /// </summary>
-//     /// <typeparam name="TRangeEnum">The enum type to be validated.</typeparam>
-//     /// <returns>True if the enum is a valid range enum, otherwise false.</returns>
-//     /// <exception cref="InvalidOperationException">Thrown if the enum does not have the <see cref="RangeEnumAttribute"/> or if it fails validation.</exception>
-//     public static bool IsValidRangeEnum<TRangeEnum>() where TRangeEnum : Enum
-//     {
-//         RangeEnumAttribute.ValidateAndThrow<TRangeEnum>(out var rangeValue);
-//
-//         var values = Enum.GetValues(typeof(TRangeEnum))
-//             .Cast<TRangeEnum>()
-//             .Select(value => Convert.ToUInt16(value))
-//             .OrderBy(value => value)
-//             .ToList();
-//
-//         return values.Zip(values.Skip(1), (prev, next) => next - prev)
-//             .All(diff => diff == rangeValue.Range);
-//     }
-//
-//     public static bool IsValidCorrelatedEnum<TType, TCategory>()
-//         where TType : Enum
-//         where TCategory : Enum
-//     {
-//         RangeEnumAttribute.ValidateAndThrow<TCategory>(out var rangeValue);
-//
-//         // Get category enum values
-//         var categoryValues = Enum.GetValues(typeof(TCategory))
-//             .Cast<TCategory>()
-//             .Select(value => Convert.ToUInt16(value))
-//             .OrderBy(value => value)
-//             .ToList();
-//
-//         // Get category enum limits
-//         var minTypeValue = categoryValues.First();
-//         var maxTypeValue = categoryValues.Last() + rangeValue.Range;
-//
-//         // Get type enum values
-//         var typeValues = Enum.GetValues(typeof(TType))
-//             .Cast<TType>()
-//             .Select(value => Convert.ToUInt16(value))
-//             .OrderBy(value => value)
-//             .ToList();
-//
-//         return typeValues.All(value => value >= minTypeValue && value < maxTypeValue);
-//     }
-//
-//     #endregion
-//
-//     #region Value Validation
-//
-//     public static bool IsValidTypeForCategory<TType, TCategory>(this TType type, TCategory category)
-//         where TType : Enum
-//         where TCategory : Enum
-//     {
-//         RangeEnumAttribute.ValidateAndThrow<TCategory>(out var rangeValue);
-//
-//         var currentCategoryValue = Convert.ToUInt16(category);
-//         var maxValue = currentCategoryValue + rangeValue.Range;
-//         var typeValue = Convert.ToUInt16(type);
-//
-//         return typeValue >= currentCategoryValue && typeValue < maxValue;
-//     }
-//
-//     public static TCategory GetCategory<TType, TCategory>(this TType type)
-//         where TType : Enum
-//         where TCategory : Enum
-//     {
-//         RangeEnumAttribute.ValidateAndThrow<TCategory>(out var rangeValue);
-//
-//         var typeValue = Convert.ToUInt16(type);
-//         var categories = Enum.GetValues(typeof(TCategory)).Cast<TCategory>();
-//
-//         var category = categories
-//             .FirstOrDefault(category =>
-//             {
-//                 var categoryValue = Convert.ToUInt16(category);
-//                 var maxValue = categoryValue + rangeValue.Range;
-//                 return typeValue >= categoryValue && typeValue < maxValue;
-//             }) ?? throw new ArgumentException(
-//             $"Type {type} doesn't belong to any category in {typeof(TCategory).Name}");
-//
-//         return category;
-//     }
-//
-//     #endregion
-// }
+using System.Reflection;
+using Backbone.Language.Core.Extensions.Enums.Range.Attributes;
+
+namespace Backbone.Language.Core.Extensions.Enums.Range.Extensions;
+
+/// <summary>
+/// Contains enum range extensions.
+/// </summary>
+public static class EnumRangeExtensions
+{
+    #region Attribute Validation
+
+    /// <summary>
+    /// Validates and retrieves the <see cref="EnumRangeAttribute"/> applied to the specified enum type.
+    /// </summary>
+    /// <typeparam name="TEnum">The enum type to retrieve enum range value from.</typeparam>
+    /// <exception cref="InvalidOperationException">Thrown when the enum does not have the <see cref="EnumRangeAttribute"/> applied.</exception>
+    public static EnumRangeAttribute GetEnumRangeValue<TEnum>() where TEnum : Enum
+    {
+        return GetEnumRangeValue(typeof(TEnum));
+    }
+
+    /// <summary>
+    /// Validates and retrieves the <see cref="EnumRangeAttribute"/> applied to the specified enum type.
+    /// </summary>
+    /// <param name="enumType">The enum type to retrieve enum range value from.</param>
+    /// <exception cref="ArgumentException">Thrown when the provided type is not an enum.</exception>
+    /// <exception cref="InvalidOperationException">Thrown when the enum does not have the <see cref="EnumRangeAttribute"/> applied.</exception>
+    public static EnumRangeAttribute GetEnumRangeValue(Type enumType)
+    {
+        if (!enumType.IsEnum)
+            throw new ArgumentException($"Type {enumType.Name} must be an enum", nameof(enumType));
+
+        if (enumType.GetCustomAttribute<EnumRangeAttribute>(false) is not { } rangeValue)
+            throw new InvalidOperationException($"Enum {enumType.Name} must be a range enum");
+
+        return rangeValue;
+    }
+
+    #endregion
+
+    #region Definition Validation
+
+    /// <summary>
+    /// Validates if the enum type is a proper range enum based on the <see cref="EnumRangeAttribute"/> applied to it.
+    /// </summary>
+    /// <typeparam name="TRangeEnum">The enum type to be validated.</typeparam>
+    /// <returns>True if the enum is a valid range enum, otherwise false.</returns>
+    /// <exception cref="InvalidOperationException">Thrown if the enum does not have the <see cref="EnumRangeAttribute"/> or if it fails validation.</exception>
+    public static bool IsValidRangeEnum<TRangeEnum>() where TRangeEnum : Enum
+    {
+        var rangeValue = GetEnumRangeValue<TRangeEnum>();
+
+        var values = Enum.GetValues(typeof(TRangeEnum))
+            .Cast<TRangeEnum>()
+            .Select(value => Convert.ToInt64(value))
+            .ToList();
+
+        return values.Zip(values.Skip(1), (prev, next) => next - prev)
+            .All(diff => diff == rangeValue.Range);
+    }
+
+    #endregion
+}
